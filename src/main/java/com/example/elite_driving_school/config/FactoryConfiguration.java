0acb6b5700d21f19af4999546c1fc9203be059b2
Krishani03 <krishanik932@gmail.com>
2025-09-18 17:28:35 +0530

    build crudutil and dbConnection

---
+++ src/main/java/com/example/elite_driving_school/config/FactoryConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.elite_driving_school.config;
+
+import com.example.elite_driving_school.entity.User;
+import com.example.elite_driving_school.entity.Student;
+import com.example.elite_driving_school.entity.Lesson;
+import com.example.elite_driving_school.entity.Instructor;
+import com.example.elite_driving_school.entity.Course;
+import com.example.elite_driving_school.entity.Payment;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.configure();//load configuration
+
+        //add entity classes
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Student.class);
+        configuration.addAnnotatedClass(Course.class);
+        configuration.addAnnotatedClass(Instructor.class);
+        configuration.addAnnotatedClass(Lesson.class);
+        configuration.addAnnotatedClass(Payment.class);
+
+        //create session factory
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ?
+                factoryConfiguration = new FactoryConfiguration() :
+                factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+}
