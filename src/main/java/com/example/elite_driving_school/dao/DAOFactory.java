e135cd6adc1ba74269a46c254552a76141184d93
Krishani03 <krishanik932@gmail.com>
2025-09-20 14:48:37 +0530

    build daoFActory

---
+++ src/main/java/com/example/elite_driving_school/dao/DAOFactory.java
@@ -0,0 +1,38 @@
+package com.example.elite_driving_school.dao;
+
+import com.example.elite_driving_school.dao.custom.impl.*;
+
+public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+    private DAOFactory() {}
+    public static DAOFactory getInstance() {
+        return (daoFactory==null)?new DAOFactory():daoFactory;
+    }
+    public enum DAOTypes {
+        USER,
+        STUDENT,
+        PAYMENT,
+        LESSON,
+        INSTRUCTOR,
+        COURSE,
+    }
+    public SuperDAO getDAO(DAOTypes daoType) {
+        switch(daoType){
+            case USER:
+                return new UserDAOImpl();
+            case STUDENT:
+                return new StudentDAOImpl();
+            case PAYMENT:
+                return new PaymentDAOImpl();
+            case LESSON:
+                return new LessonDAOImpl();
+            case INSTRUCTOR:
+                return new InstructorDAOImpl();
+            case COURSE:
+                return new CourseDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
