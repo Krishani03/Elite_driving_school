d461260f65b5d38a9ac96548e2bf08b765512453
Krishani03 <krishanik932@gmail.com>
2025-09-23 18:44:15 +0530

    build login form ui

---
+++ src/main/java/com/example/elite_driving_school/controller/Login_FormController.java
@@ -0,0 +1,27 @@
+package com.example.elite_driving_school.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Login_FormController {
+    @FXML
+    private AnchorPane LoginPane;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLogin_Onaction(ActionEvent event) {
+
+    }
+}
