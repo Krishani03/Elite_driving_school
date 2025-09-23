1ed86c20e072e2ad6bd5ac0d5a6070987c8ad8c0
Krishani03 <krishanik932@gmail.com>
2025-09-23 23:14:06 +0530

    create adminForm, loginForm and recepForm for ui

---
+++ src/main/java/com/example/elite_driving_school/controller/Admin_FormController.java
@@ -0,0 +1,59 @@
+package com.example.elite_driving_school.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Admin_FormController {
+    @FXML
+    private AnchorPane ancMainContainer;
+
+    @FXML
+    private Button btnCourse;
+
+    @FXML
+    private Button btnInstruc;
+
+    @FXML
+    private Button btnLesson;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    private Button btnUser;
+
+    @FXML
+    void btnCourseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnInstructorOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLessonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserOnAction(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/com/example/elite_driving_school/controller/Recep_FormController.java
@@ -0,0 +1,35 @@
+package com.example.elite_driving_school.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Recep_FormController {
+    @FXML
+    private AnchorPane ancMainContainer;
+
+    @FXML
+    private Button btnLesson;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    void btnLessonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+}
